55d10ecd5731ab12a38e3c9af8017a366b43bb63
Evelina <eushakova300@gmail.com>
2025-02-03 16:05:27 +0300

    Second change

--- sima_memorial/memorial/urls.py
+++ sima_memorial/memorial/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from memorial.views import index, about_page, articles
+from memorial.views import (index, about_page, articles, redirect_to_story_1,
+                            redirect_to_story_2, redirect_to_story_3, redirect_to_story_4)
 urlpatterns = [
     path('', index),
     path('about/', about_page),
-    path('article/<int:number>', articles)
+    path('article/<int:number>', articles),
+    path('redirect-to-story-1/', redirect_to_story_1, name='redirect_to_story_1'),
+    path('redirect-to-story-2/', redirect_to_story_2, name='redirect_to_story_2'),
+    path('redirect-to-story-3/', redirect_to_story_3, name='redirect_to_story_3'),
+    path('redirect-to-story-4/', redirect_to_story_4, name='redirect_to_story_4')
 ]
